372d3d91cf4be61e7374e5b165d40f1700ab6828
manuelarc06 <mrodriguezcardenas@byupathway.edu>
2026-09-11 23:26:38 -0500

    Add remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -8,6 +8,7 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <button class="remove-item" data-id="${item.Id}" type="button">X</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -24,5 +25,25 @@ function cartItemTemplate(item) {
 
   return newItem;
 }
+
+function removeFromCart(event) {
+  if (!event.target.classList.contains("remove-item")) {
+    return;
+  }
+
+  const productId = event.target.dataset.id;
+
+  const cartItems = getLocalStorage("so-cart");
+
+  const updatedCart = cartItems.filter((item) => item.Id !== productId);
+
+  setLocalStorage("so-cart", updatedCart);
+
+  renderCartContents();
+}
+
+document
+  .querySelector(".product-list")
+  .addEventListener("click", removeFromCart);
 
 renderCartContents();
