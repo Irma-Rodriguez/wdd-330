18ce53979d0dd7ced413217f2c927174e9b92ec1
Irma-Rodriguez <irodriguezcardenas@byupathway.edu>
2026-09-16 23:33:04 -0500

    Dynamic Header and Footer activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,61 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+loadHeaderFooter();
 
-  if (cartItems.length > 0) {
-    const cartFooter = document.querySelector(".cart-footer");
-    cartFooter.classList.remove("hide");
+const cart = new ShoppingCart(document.querySelector(".product-list"));
 
-    const total = cartItems.reduce(
-      (sum, item) => sum + Number(item.FinalPrice),
-      0,
-    );
-    document.querySelector(".cart-total").innerHTML =
-      `Total: $${total.toFixed(2)}`;
-  }
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <button class="remove-item" data-id="${item.Id}" type="button">X</button>
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-function removeFromCart(event) {
-  if (!event.target.classList.contains("remove-item")) {
-    return;
-  }
-
-  const productId = event.target.dataset.id;
-
-  const cartItems = getLocalStorage("so-cart");
-
-  const updatedCart = cartItems.filter((item) => item.Id !== productId);
-
-  setLocalStorage("so-cart", updatedCart);
-
-  renderCartContents();
-}
-
-document
-  .querySelector(".product-list")
-  .addEventListener("click", removeFromCart);
-
-renderCartContents();
+cart.init();
