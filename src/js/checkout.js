c62e003bfbbc1c8acdead64fafe5f09273cc8887
manuelarc06 <mrodriguezcardenas@byupathway.edu>
2026-09-23 22:12:16 -0500

    add checkout totals and form

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,17 @@
 import { loadHeaderFooter } from './utils.mjs';
+import CheckoutProcess from './CheckoutProcess.mjs';
 
 loadHeaderFooter();
+
+const checkout = new CheckoutProcess(
+    'so-cart',
+    '.products'
+);
+
+checkout.init();
+
+const zipInput = document.querySelector('#zip');
+
+zipInput.addEventListener('blur', () => {
+    checkout.calculateOrderTotal();
+});
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,30 +1,30 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { loadHeaderFooter, getParam } from './utils.mjs';
 
 loadHeaderFooter();
 
-const category = getParam("category");
+const category = getParam('category');
 
-const titleElement = document.querySelector("#product-title");
+const titleElement = document.querySelector('#product-title');
 
 const categoryName = category
-  .split("-")
+  .split('-')
   .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  .join(" ");
+  .join(' ');
 
 titleElement.textContent = `Top Products: ${categoryName}`;
 
 const dataSource = new ProductData();
 
-const listElement = document.querySelector(".product-list");
+const listElement = document.querySelector('.product-list');
 
 const myList = new ProductList(category, dataSource, listElement);
 
 myList.init();
 
-const sortSelect = document.querySelector("#sort-select");
+const sortSelect = document.querySelector('#sort-select');
 
-sortSelect.addEventListener("change", (event) => {
+sortSelect.addEventListener('change', (event) => {
   myList.sortList(event.target.value);
 });
